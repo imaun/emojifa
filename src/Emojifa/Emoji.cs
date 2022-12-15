ea7a8e33cd1cc30a1c32e63f21ed2304676e0ff5
imaun <imun22@gmail.com>
2022-12-15 17:18:29 +0330

    Add Simple Extensions

--- src/Emojifa/Emoji.cs
+++ src/Emojifa/Emoji.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Emojifa
+﻿namespace Emojifa
 {
     public static partial class Emoji
     {
---
+++ src/Emojifa/Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Emojifa
+{
+    public static partial class Emoji
+    {
+
+        public static EmojiData[] FindByTitle(string title) {
+            if (string.IsNullOrWhiteSpace(title))
+                throw new ArgumentNullException(nameof(title));
+
+            return Emoji.All.Where(
+                _ => _.Title.ToUpper().Contains(title.ToUpper())).ToArray();
+        }
+
+
+        public static string[] FindRawEmojiesByTitle(string title) {
+            if (string.IsNullOrWhiteSpace(title))
+                throw new ArgumentNullException(nameof(title));
+
+            return Emoji.All.Where(_ => _.Title.ToUpper().Contains(title.ToUpper()))
+                                    .Select(_=> _.Raw).ToArray();
+        }
+
+
+        public static string ConvertHexCodesToEmoji(string text) {
+            return string.Empty;
+        }
+    }
+}
