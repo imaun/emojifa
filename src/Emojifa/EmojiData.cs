d0ab7fa3451c43a887b32fa803caeb6370c05820
imaun <imun22@gmail.com>
2022-12-15 04:51:26 +0330

    initial commit

---
+++ src/Emojifa/EmojiData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Emojifa
+{
+
+    public class EmojiData
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Raw { get; set; }
+        public string? Category { get; set; }
+        public string[] Aliases { get; set; }
+        public string[]? Tags { get; set; }
+        public string? UnicodeVersion { get; set; }
+        public string? IosVersion { get; set; }
+        public string[]? PersianNames { get; set; }
+    }
+}
