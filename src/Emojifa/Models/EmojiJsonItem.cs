2ac229a7fe967894933a24ab50ea5a5afc36c790
imun <imun22@gmail.com>
2022-12-17 16:29:43 +0330

    Add Emoji.json and add .idea to gitignore

---
+++ src/Emojifa/Models/EmojiJsonItem.cs
@@ -0,0 +1,14 @@
+namespace Emojifa.Models
+{
+    internal class EmojiJsonItem
+    {
+
+        public EmojiJsonItem()
+        {
+        }
+        
+        public string emoji { get; set; }
+        public string description { get; set; }
+        public string category { get; set; }
+    }
+}
